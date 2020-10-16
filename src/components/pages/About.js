bed0ac574de1f9519a0815d2a5be8bc38b5b4893
Lin <linmaweb@gmail.com>
2020-10-16 11:51:42 +1000

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sequi, id assumenda eveniet iure repellat aperiam aliquid reiciendis ullam in recusandae, dolorum laborum commodi laboriosam asperiores. Itaque ullam a tempore?</p>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/pages/Terms.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Terms = () => {
+  return (
+    <>
+      <h1>Terms </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sequi, id
+        assumenda eveniet iure repellat aperiam aliquid reiciendis ullam in
+        recusandae, dolorum laborum commodi laboriosam asperiores. Itaque ullam
+        a tempore?
+      </p>
+    </>
+  );
+};
+
+export default Terms;
